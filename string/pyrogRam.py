ecd22572ff100a8822469ebe223b849bfd991d82
izzy <50057075+hitokizzy@users.noreply.github.com>
2022-03-18 21:21:40 +0700

    Update pyrogRam.py

--- string/pyrogRam.py
+++ string/pyrogRam.py
@@ -12,3 +12,33 @@ SUKASUKA = [
     "jangan di pake dulu lah kawan"
     "belom di update wlee"
 ]
+
+
+ram_a = "ini cuma module tester, belom diapa apain, paham kan"
+ram_b = "tunggu izzy sama ram sama piki update dulu yah"
+ram_c = "ini cuma module tester, belom diapa apain, paham kan"
+ram_d = "tunggu izzy sama ram sama piki update dulu yah"
+ram_e = "ini cuma module tester, belom diapa apain, paham kan"
+ram_f = "tunggu izzy sama ram sama piki update dulu yah"
+ram_g = "ini cuma module tester, belom diapa apain, paham kan"
+ram_h = "tunggu izzy sama ram sama piki update dulu yah"
+ram_i = "ini cuma module tester, belom diapa apain, paham kan"
+ram_j = "tunggu izzy sama ram sama piki update dulu yah"
+ram_k = "ini cuma module tester, belom diapa apain, paham kan"
+ram_l = "tunggu izzy sama ram sama piki update dulu yah"
+ram_m = "tunggu izzy sama ram sama piki update dulu yah"
+ram_n = "ini cuma module tester, belom diapa apain, paham kan"
+ram_o = "tunggu izzy sama ram sama piki update dulu yah"
+ram_p = "ini cuma module tester, belom diapa apain, paham kan"
+ram_q = "tunggu izzy sama ram sama piki update dulu yah"
+ram_r = "tunggu izzy sama ram sama piki update dulu yah"
+ram_s = "ini cuma module tester, belom diapa apain, paham kan"
+ram_t = "tunggu izzy sama ram sama piki update dulu yah"
+ram_u = "ini cuma module tester, belom diapa apain, paham kan"
+ram_v = "tunggu izzy sama ram sama piki update dulu yah"
+ram_w = "tunggu izzy sama ram sama piki update dulu yah"
+ram_x = "ini cuma module tester, belom diapa apain, paham kan"
+ram_y = "tunggu izzy sama ram sama piki update dulu yah"
+ram_z = "ini cuma module tester, belom diapa apain, paham kan"
+
+
