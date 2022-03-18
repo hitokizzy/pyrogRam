2daa0aaf3181141f09e33394e00e38b7220a9bdf
Garamijo <101814588+Garamijo@users.noreply.github.com>
2022-03-19 06:34:45 +0700

    Update __init__.py

--- string/__init__.py
+++ string/__init__.py
@@ -1,1 +1,26 @@
 from .pyrogRam import *
+
+
+def __list_all_string():
+
+    from os.path import dirname, basename, isfile
+
+    import glob
+
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+
+    all_string = [
+
+        basename(f)[:-3] for f in mod_paths
+
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+
+    ]
+
+    return all_string
+
+ALL_STRING = sorted(__list_all_string())
+
+LOGS.info("String to load: %s", str(ALL_STRING))
+
+__all__ = ALL_MODULES + ["ALL_STRING"]
