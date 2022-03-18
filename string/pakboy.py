8022b277d44465c52c9ac2b5721555cab769ab14
Garamijo <101814588+Garamijo@users.noreply.github.com>
2022-03-19 06:01:11 +0700

    Create pakboy.py

---
+++ string/pakboy.py
@@ -0,0 +1,44 @@
+#pakboy
+PAKBOY = [
+    "AKU CUMA MAU AMA KAMU DOANG ",
+    " DI TAMBAH KAMU TUH CANTIK",
+    "Itu lah PAKBOY kelas mengontol.",
+]
+    
+
+#GENIT
+GENIT = [
+    "AKU MEMANG MANUSIA BIASA ",
+    " YANG TAK SEMPURNA DAN KADANG SALAH",
+    "NAMUN DI HATIKU HANYA SATU CINTA UNTUKMU, LUAR BINASA",
+]
+
+
+ram_ab = "ANDAIKA AKU BISA MENDAPATKANMU...."
+ram_bc = "BOLEH GAK SI AKU NIH BERUSAHA MENDAPATKAN SESEORANG,YAITU KAMU..."
+ram_cd = "CUMA MAU NGUNGKAPIN PERASAAN AJA SUSAHNYA MINTA APUN KE KAMU"
+ram_de = "DULU PERNAH KU MENJADI SASIMO DAN PAKBOY, GAK TAHU KENAPA DAPET KAMU AKU HANYA INGIN BERSAMA KAMU AJA"
+ram_ef = "aku akan selalu menyukaimu" 
+ram_fg = "JANGAN LUPA UNTUK HAPPY KIYOWO DI SETIAP HARINYA KAKAKK :)"
+ram_gh = "JIKA MEMANG AKU TIDAK BISA MENDAPAT KAMU,TOLONG DI PERHATIKAN KEBERADAAN DIRIKU YANG DULU PERNAH MENGISI KEGABUTAN KAMU DENGAN SEMUA KEBODOHAN YANG KU PUNYA"
+ram_hi = "AWASS NANTI JADI SUKAA !"
+ram_ij = "RUN!....LARII ADA YANG BUCINN !"
+ram_jk = "JIMATT PEMIKAT CEWEEE....CIATTTT"
+ram_kl = "kalo kita selalu tiba-tiba **CINLOK** BAGAIMANA?"
+ram_lm = "Wa'alaikumsalam, ada apa cantik?"
+ram_mn = "MASA LALU ITU HARUS DI INGET KAK,SUPAYA NANTI KALO TIBA-TIBA KITA LUPA DEKETIN ORANG KAN BISA DI INGET KMRN KITA PDKTNYA GIMANA HEHEHE"
+ram_no = "NIH DIA YANG GW TUNGGUIN DARI KEMAREN, DATENG JUGA SI BABU WUAHAHAHA"
+ram_op = "Oh jadi ini yang sering membuat kau selalu ada di pikiran aku?"
+ram_pq = "Assalamu'alaikum"
+ram_qr = "kamu tersakiti, dan aku pun juga tersakiti, bagaimana kalo kita saling melengkapi"
+ram_rs = "rasa itu selalu timbul ketika kita saling tertawa, chat, berbicara dan aku selalu menginginkan dirimu saja"
+ram_st = "sayang?? gorengannya jatoh tuh belom 5 menit. Sayang-sayang kalo di buang:V"
+ram_tu = "aku yang dulu bukan aku yang sekarang, karena aku bisa merubah kekurangan aku menjadi sesuatu kelebihan yang ku miliki"
+ram_uv = "Ciee yang lagi menyukai tetapi tidak tau mau berbuat apa"
+ram_vw = "aku bukan Joker kak, yang abis tersakiti lalu jahat hehehe"
+ram_wx = "kata orang itu **JANGAN JADI PELANGI UNTUK ORANG YANG BUTA WARNA**, kalo kata aku si mending jadi awan aja kak yang gak tau apa apa wkwk"
+ram_xy = "kalo di free fire ada **ONESHOOT**, kalo di sini ada **ONE HEART**"
+ram_yz = "ternyata oh ternyata bener kata orang, **KALO JODOH TUH GAK AKAN KEMANA**"
+ram_z1 = "jangan mau jadi SCTV kak, nanti satu untuk semua jatohnya nanti jadi **LONTE**"
+
+
